15e5e8d33e71eee0fa55765209a48374eb0c6182
小小的大肥 <11911839@mail.sustech.edu.cn>
2022-08-17 21:59:21 +0800

    Add pause count down

--- Assets/Scripts/UI/DancingLineGameManager.cs
+++ Assets/Scripts/UI/DancingLineGameManager.cs
@@ -1,6 +1,8 @@
-﻿using DefaultNamespace;
+﻿using System.Collections;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.UI;
+using Utils;
 
 namespace UI
 {
@@ -11,10 +13,14 @@ namespace UI
         public GameObject loseCanvas;
         public GameObject pauseCanvas;
         public GameObject restartCanvas;
-        
-        public Toggle toggle;
-        public Button restart;
-        
+        public GameObject countDownCanvas;
+
+        public Toggle toggle; // 暂停/开始 toggle
+        public Button restart; // 重新开始btn
+        public Text countDown; // 倒计时
+        public AudioSource btnEffect; // 按钮音效
+        public AudioSource countdownEffect; // 倒计时音效
+
         public static DancingLineGameManager Instance;
 
         private void Awake()
@@ -35,6 +41,9 @@ namespace UI
                 toggle.enabled = false;
             });
         }
+        
+        
+        
 
         public void OnClickGameStart()
         {
@@ -47,6 +56,7 @@ namespace UI
             loseCanvas.SetActive(false);
             pauseCanvas.SetActive(false);
             restartCanvas.SetActive(false);
+            countDownCanvas.SetActive(false);
         }
 
         public void OnOpenMainCanvas()
@@ -56,6 +66,7 @@ namespace UI
             loseCanvas.SetActive(false);
             pauseCanvas.SetActive(false);
             restartCanvas.SetActive(false);
+            countDownCanvas.SetActive(false);
         }
 
 
@@ -66,28 +77,56 @@ namespace UI
             gameCanvas.SetActive(false);
             pauseCanvas.SetActive(false);
             restartCanvas.SetActive(false);
+            countDownCanvas.SetActive(false);
         }
 
         public void OnClickRestartBtn()
         {
             GameObject.Find("Line").gameObject.GetComponent<LineController>().bPause = false;
             GameObject.Find("Line").gameObject.GetComponent<LineController>().bStart = false;
+            btnEffect.Play();
             OnOpenMainCanvas();
         }
 
         public void OnClickCancelRestart()
         {
-            GameObject.Find("Line").gameObject.GetComponent<LineController>().bPause = false;
-            restartCanvas.SetActive(false);
-            toggle.enabled = true;
+            StartCoroutine(PauseCountDown());
         }
 
         public void OnClickPauseToggle(Toggle t, bool isOn)
         {
-            GameObject.Find("Line").gameObject.GetComponent<LineController>().bPause = isOn;
-            pauseCanvas.SetActive(isOn);
-            restart.enabled = !isOn;
+            if (!isOn)
+            {
+                StartCoroutine(PauseCountDown());
+            }
+            else
+            {
+                GameObject.Find("Line").gameObject.GetComponent<LineController>().bPause = true;
+                pauseCanvas.SetActive(true);
+                restart.enabled = false;
+                btnEffect.Play();
+            }
         }
-        
+
+
+        IEnumerator PauseCountDown()
+        {
+            restartCanvas.SetActive(false);
+            pauseCanvas.SetActive(false);
+            countDownCanvas.SetActive(true);
+            toggle.enabled = false;
+            restart.enabled = false;
+            for (int i = 3; i > 0; i--)
+            {
+                countDown.text = i.ToString();
+                countdownEffect.Play();
+                yield return new WaitForSeconds(1f);
+            }
+
+            GameObject.Find("Line").gameObject.GetComponent<LineController>().bPause = false;
+            countDownCanvas.SetActive(false);
+            toggle.enabled = true;
+            restart.enabled = true;
+        }
     }
 }
